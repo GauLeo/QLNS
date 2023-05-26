fb7653f513be2480be8833e64581fec5b82b0c91
GauLeo <anhtuan01697@gmail.com>
2023-05-26 19:29:50 +0700

    QLNS

---
+++ QuanLyNhanSu/frmLogin.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyNhanSu
+{
+    public partial class frmLogin : Form
+    {
+
+        #region Variables
+        public static string ConnectionString = "Data Source = ANHTUAN\SQLEXPRESS; Initial Catalog = QLNS; User ID = sa; Password = 1";
+        #endregion
+        public frmLogin()
+        {
+            InitializeComponent();
+            lblError.Text = "";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblError_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                lblError.Text = "";
+                //Kiểm tra xem textbox tài khoản, textbox mật khẩu đã nhập hay chưa
+                if (txtTenTKhoan.Text = null && txtTenTKhoan.Text.Trim() != "") { }
+                else
+                {
+                    MessageBox.Show("Chưa nhập thông tin Tên Tài Khoản", "Thông báo");
+                    txtTenTKhoan.Focus();
+                    return;
+                }
+
+                if (txtMatKhau.Text != null && txtMatKhau.Text.Trim() != "") { }
+                else
+                {
+                    MessageBox.Show("Chưa nhập thông tin Mật Khẩu", "Thông Báo");
+                    txtMatKhau.Focus();
+                    return;
+                }
+
+                //Kiểm tra thông tin tài khoản so sánh với thông tin bảng tblTaiKhoan (trong dữ liệu)
+                SqlConnection conn = new SqlConnection(ConnectionString);
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                string TenTKhoan = txtTenTKhoan.Text.Trim();
+                string MatKhau = txtMatKhau.Text.Trim();
+                string query = "SELECT * FROM tblTaiKhoan WHERE Ten_TKhoan = '" + TenTKhoan + "' AND Mat_Khau = '" + MatKhau + "'";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    frmMain _frmMain = new frmMain();
+                    _frmMain.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    //Khong tìm thấy
+                    lblError.Text = "Thông tin tài khoản hoặc mật khẩu chưa chính xác.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+            }
+        }
+    }
+}
---
+++ QuanLyNhanSu/frmMain.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace QuanLyNhanSu
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.danhMụcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.chứcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.phòngBanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýHồSơToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.khaiBáoHồSơNhânSựToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnTrịHệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cấuHìnhHệThốngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýTàiKhoảnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quảnLýChứcNăngToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.danhMụcToolStripMenuItem,
+            this.quảnLýHồSơToolStripMenuItem,
+            this.quảnTrịHệThốngToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // danhMụcToolStripMenuItem
+            // 
+            this.danhMụcToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chứcToolStripMenuItem,
+            this.phòngBanToolStripMenuItem});
+            this.danhMụcToolStripMenuItem.Name = "danhMụcToolStripMenuItem";
+            this.danhMụcToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.danhMụcToolStripMenuItem.Text = "Danh mục";
+            // 
+            // chứcToolStripMenuItem
+            // 
+            this.chứcToolStripMenuItem.Name = "chứcToolStripMenuItem";
+            this.chứcToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.chứcToolStripMenuItem.Text = "Chức Vụ";
+            // 
+            // phòngBanToolStripMenuItem
+            // 
+            this.phòngBanToolStripMenuItem.Name = "phòngBanToolStripMenuItem";
+            this.phòngBanToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.phòngBanToolStripMenuItem.Text = "Phòng Ban";
+            // 
+            // quảnLýHồSơToolStripMenuItem
+            // 
+            this.quảnLýHồSơToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.khaiBáoHồSơNhânSựToolStripMenuItem});
+            this.quảnLýHồSơToolStripMenuItem.Name = "quảnLýHồSơToolStripMenuItem";
+            this.quảnLýHồSơToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.quảnLýHồSơToolStripMenuItem.Text = "Quản lý hồ sơ";
+            // 
+            // khaiBáoHồSơNhânSựToolStripMenuItem
+            // 
+            this.khaiBáoHồSơNhânSựToolStripMenuItem.Name = "khaiBáoHồSơNhânSựToolStripMenuItem";
+            this.khaiBáoHồSơNhânSựToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.khaiBáoHồSơNhânSựToolStripMenuItem.Text = "Khai báo hồ sơ nhân sự";
+            // 
+            // quảnTrịHệThốngToolStripMenuItem
+            // 
+            this.quảnTrịHệThốngToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cấuHìnhHệThốngToolStripMenuItem,
+            this.quảnLýTàiKhoảnToolStripMenuItem,
+            this.quảnLýChứcNăngToolStripMenuItem});
+            this.quảnTrịHệThốngToolStripMenuItem.Name = "quảnTrịHệThốngToolStripMenuItem";
+            this.quảnTrịHệThốngToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
+            this.quảnTrịHệThốngToolStripMenuItem.Text = "Quản trị hệ thống";
+            // 
+            // cấuHìnhHệThốngToolStripMenuItem
+            // 
+            this.cấuHìnhHệThốngToolStripMenuItem.Name = "cấuHìnhHệThốngToolStripMenuItem";
+            this.cấuHìnhHệThốngToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cấuHìnhHệThốngToolStripMenuItem.Text = "Cấu hình hệ thống";
+            // 
+            // quảnLýTàiKhoảnToolStripMenuItem
+            // 
+            this.quảnLýTàiKhoảnToolStripMenuItem.Name = "quảnLýTàiKhoảnToolStripMenuItem";
+            this.quảnLýTàiKhoảnToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quảnLýTàiKhoảnToolStripMenuItem.Text = "Quản lý tài khoản";
+            // 
+            // quảnLýChứcNăngToolStripMenuItem
+            // 
+            this.quảnLýChứcNăngToolStripMenuItem.Name = "quảnLýChứcNăngToolStripMenuItem";
+            this.quảnLýChứcNăngToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.quảnLýChứcNăngToolStripMenuItem.Text = "Quản lý chức năng";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 26);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Quản lý hồ sơ nhân sự";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem danhMụcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem chứcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem phòngBanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýHồSơToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem khaiBáoHồSơNhânSựToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnTrịHệThốngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cấuHìnhHệThốngToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýTàiKhoảnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quảnLýChứcNăngToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
